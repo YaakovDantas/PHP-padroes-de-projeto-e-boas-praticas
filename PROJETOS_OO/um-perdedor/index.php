73f0f3d79cfb51cd4663da4e2265119df4256f66
Raiany Rufino <raiany.paz@ccc.ufcg.edu.br>
2022-10-04 10:28:11 -0300

    Update index.php

--- PROJETOS_OO/um-perdedor/index.php
+++ PROJETOS_OO/um-perdedor/index.php
@@ -11,8 +11,6 @@ $jogoZeroUm = new JogoZeroUm("Huguinho", "Zezinho", "Luisinho");
 $jogoZeroUm->pegarVencedor();
 $perdedores = $jogoZeroUm->pegarPerdedores();
 
-// var_dump($perdedores);
-
 $jogador1 = $perdedores[0];
 $jogador2 = $perdedores[1];
 $jogador2->setOpostoParOuImpar($jogador1->getEscolhaParOuImpar());
